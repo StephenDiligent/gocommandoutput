447c06ac9e2309861304361a5fd56c46eaba8e62
Stephen Payne <spayne@diligent.com>
2020-10-21 10:57:17 +1300

    Test command output on Linux

--- main.go
+++ main.go
@@ -15,15 +15,13 @@ type command struct {
 
 func main() {
 	commands := make(map[int]command)
-	commands[1] = command{name: "cmd", args: []string{"/C", "echo", "helloworld"}, input: "helloworld"}
-	commands[2] = command{name: "powershell", args: []string{"echo", "helloworld"}, input: "helloworld"}
+	commands[1] = command{name: "echo", args: []string{"-n", "helloworld"}, input: "helloworld"}
+	commands[2] = command{name: "powershell", args: []string{"-Command", "echo", "helloworld"}, input: "helloworld"}
 	commands[3] = command{name: "more", args: []string{"helloworld.txt"}, input: "helloworld"}
-	commands[4] = command{name: "cmd", args: []string{"/C", "more", "helloworld.txt"}, input: "helloworld"}
 
-	commands[5] = command{name: "cmd", args: []string{"/C", "echo", "hello world"}, input: "hello world"}
-	commands[6] = command{name: "powershell", args: []string{"echo", "hello world"}, input: "hello world"}
+	commands[5] = command{name: "echo", args: []string{"-n", "hello world"}, input: "hello world"}
+	commands[6] = command{name: "powershell", args: []string{"-Command", "echo", "hello world"}, input: "hello world"}
 	commands[7] = command{name: "more", args: []string{"hello world.txt"}, input: "hello world"}
-	commands[8] = command{name: "cmd", args: []string{"/C", "more", "hello world.txt"}, input: "hello world"}
 
 	for id, command := range commands {
 		res, err := exec.Command(command.name, command.args...).Run()
